a2ccb4be5dc75e1706c1676c6c8b0c9c1266d58f
Andrey <info@rugento.ru>
2017-02-12 21:34:21 +0300

    [+] start

---
+++ app/code/Ioncube/Di/Code/Reader/ClassReader.php
@@ -0,0 +1,47 @@
+<?php
+namespace Ioncube\Di\Code\Reader;
+
+class ClassReader extends \Magento\Framework\Code\Reader\ClassReader
+{
+    /**
+     * Read class constructor signature
+     *
+     * @param string $className
+     * @return array|null
+     * @throws \ReflectionException
+     */
+    public function getConstructor($className)
+    {
+        $class       = new \ReflectionClass($className);
+        $result      = null;
+        $constructor = $class->getConstructor();
+        if ($constructor) {
+            $result = [];
+
+           //XXX
+            if($class->hasMethod('__precompile')) {
+              return $class->getMethod('__precompile')->invoke(null);
+            }
+            //XXX
+
+            /** @var $parameter \ReflectionParameter */
+            foreach ($constructor->getParameters() as $parameter) {
+                try {
+                    $result[] = [
+                        $parameter->getName(),
+                        $parameter->getClass() !== null ? $parameter->getClass()->getName() : null,
+                        !$parameter->isOptional(),
+                        $parameter->isOptional()
+                        ? ($parameter->isDefaultValueAvailable() ? $parameter->getDefaultValue() : null)
+                        : null,
+                    ];
+                } catch (\ReflectionException $e) {
+                    $message = $e->getMessage();
+                    throw new \ReflectionException($message, 0, $e);
+                }
+            }
+        }
+
+        return $result;
+    }
+}
